b0260a66ba9d12205cb7111b30ef96b285f0794c
Kirin Ly <nightfish9999@gmail.com>
2023-07-10 23:32:32 +0700

    add Discount.Grpc

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -19,10 +19,8 @@ namespace Discount.Grpc
         builder.Services.AddAutoMapper(typeof(Program));
         builder.Services.AddScoped<IDiscountContext, DiscountContext>();
         builder.Services.AddScoped<ICouponRepository, CouponRepository>();
-
         var app = builder.Build();
         app.MigrateData();
-
         // Configure the HTTP request pipeline.
         app.MapGrpcService<CouponService>();
         app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
